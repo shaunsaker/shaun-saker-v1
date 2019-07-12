86471ab52e324273dcb2a73e40c81275dcf35cfb
Shaun Saker <shaun@aux.co.za>
2019-07-12 16:50:01 +0200

    Add icons

--- src/app/components/Icon/icons.js
+++ src/app/components/Icon/icons.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { AccountCircle, Add, ExpandMore, MoreVert, Share, Sort } from '@material-ui/icons';
+import { ExpandMore, Mail, MoreVert, Phone, PersonOutline } from '@material-ui/icons';
 
 const icons = {
-  'account-circle': (props) => <AccountCircle {...props} />,
-  add: (props) => <Add {...props} />,
   'expand-more': (props) => <ExpandMore {...props} />,
+  mail: (props) => <Mail {...props} />,
   menu: (props) => <MoreVert {...props} />,
-  share: (props) => <Share {...props} />,
-  sort: (props) => <Sort {...props} />,
+  phone: (props) => <Phone {...props} />,
+  person: (props) => <PersonOutline {...props} />,
 };
 
 export default icons;
