51e3b75f05e3af113b6000fba8759be29d50c4ca
Shaun Saker <shaun@aux.co.za>
2019-07-13 10:06:48 +0200

    Home/Skills section uses Section component

--- src/app/layouts/Home/Skills/index.js
+++ src/app/layouts/Home/Skills/index.js
@@ -3,26 +3,24 @@ import PropTypes from 'prop-types';
 
 import styles from './styles';
 
-import Typography from '../../../components/Typography';
+import Section from '../../../components/Section';
 import Label from '../../../components/Label';
 
 const Skills = ({ skills }) => {
   return (
     <div className="container">
-      <div className="title-text-container">
-        <Typography type="title">Skills</Typography>
-      </div>
-
-      <div className="skill-items-container">
-        {skills &&
-          skills.map((item) => {
-            return (
-              <div key={item.name} className="skill-item-container">
-                <Label>{item.name}</Label>
-              </div>
-            );
-          })}
-      </div>
+      <Section title="Skills">
+        <div className="skill-items-container">
+          {skills &&
+            skills.map((item) => {
+              return (
+                <div key={item.name} className="skill-item-container">
+                  <Label>{item.name}</Label>
+                </div>
+              );
+            })}
+        </div>
+      </Section>
 
       <style jsx>{styles}</style>
     </div>
--- src/app/layouts/Home/Skills/styles.js
+++ src/app/layouts/Home/Skills/styles.js
@@ -1,14 +1,9 @@
 import css from 'styled-jsx/css'; // eslint-disable-line
 
-import { rhythm, sizes } from '../../../static/styles/styleConstants';
+import { rhythm } from '../../../static/styles/styleConstants';
 
 const styles = css`
   .container {
-    padding: ${rhythm.vt * 2}px ${rhythm.hz}px ${rhythm.vt}px;
-  }
-
-  .title-text-container {
-    margin-bottom: ${rhythm.vt * 2}px;
   }
 
   .skill-items-container {
@@ -17,12 +12,6 @@ const styles = css`
 
   .skill-item-container {
     margin: 0 ${rhythm.hz / 2}px ${rhythm.vt}px 0;
-  }
-
-  @media (min-width: ${sizes.maxContentWidth}px) {
-    .container {
-      padding: ${rhythm.vt * 4}px ${rhythm.hz * 2}px;
-    }
   }
 `;
 
