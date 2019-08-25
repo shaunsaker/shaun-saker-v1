ee55e2ed9fd9b333143d0df6e0763e2e65dccf6c
Shaun Saker <shaun@aux.co.za>
2019-08-25 11:41:39 +0200

    PrimaryButton/Fix stories

--- src/app/components/PrimaryButton/__stories__/index.stories.js
+++ src/app/components/PrimaryButton/__stories__/index.stories.js
@@ -5,7 +5,7 @@ import PrimaryButton from '..';
 
 storiesOf('Components|PrimaryButton', module)
   .add('default', () => <PrimaryButton>DEFAULT</PrimaryButton>)
-  .add('with icon', () => <PrimaryButton iconName="add">WITH ICON</PrimaryButton>)
+  .add('with icon', () => <PrimaryButton iconName="mail">WITH ICON</PrimaryButton>)
   .add('secondary', () => <PrimaryButton secondary>SECONDARY</PrimaryButton>)
   .add('small', () => <PrimaryButton small>SMALL</PrimaryButton>)
   .add('ghost', () => <PrimaryButton ghost>GHOST</PrimaryButton>)
