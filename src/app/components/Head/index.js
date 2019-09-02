c0e339b0e45e29abd7de2ea37cbc114c3e9cf262
Shaun Saker <shaun@aux.co.za>
2019-09-02 06:20:08 +0200

    Add Hotjar

--- src/app/components/Head/index.js
+++ src/app/components/Head/index.js
@@ -11,6 +11,16 @@ const primaryColor = colors.primary;
 const defaultTitle = SEO.title;
 const defaultDescription = SEO.description;
 
+const hotjarScript = `
+  (function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:1467901,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`;
+
 const HeadComponent = ({ title, description, router }) => {
   const { pathname } = router;
   const routesArray = convertObjectToArray(routes);
@@ -57,6 +67,9 @@ const HeadComponent = ({ title, description, router }) => {
 
       {/* Normalize css */}
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+
+      {/* Hotjar */}
+      <script dangerouslySetInnerHTML={{ __html: hotjarScript }} />
 
       <noscript>Your browser does not support JavaScript!</noscript>
     </Head>
