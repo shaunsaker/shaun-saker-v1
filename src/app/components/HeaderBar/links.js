0cb63e636ca76b10be575e95eb8195971cc23257
Shaun Saker <shaun@aux.co.za>
2019-09-24 12:46:52 +0200

    Replace local resume with remote

--- src/app/components/HeaderBar/links.js
+++ src/app/components/HeaderBar/links.js
@@ -2,7 +2,11 @@ const links = [
   { name: 'Skills', href: '#skills', id: 'skills' },
   { name: 'Projects', href: '#projects', id: 'projects' },
   { name: 'Contact', href: '#contact', id: 'contact' },
-  { name: 'Resume', href: '/static/documents/resume.pdf', target: '_blank' },
+  {
+    name: 'Resume',
+    href: 'https://drive.google.com/file/d/1wnxswZ--Wx_ph65HEsNK2UQBtIcBfEd2/view?usp=sharing',
+    target: '_blank',
+  },
 ];
 
 export default links;
